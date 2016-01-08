786da3ed29c1f1d856deeaea9875494624d46b5f
Patrick Flynn <patflynn@hotmail.com>
2016-01-07 18:22:31 -0800

    Lesson 1-3 final reorg

--- Lesson3/l2vecnormBackup.c
+++ Lesson3/l2vecnormBackup.c
@@ -17,7 +17,7 @@ int main( int argc, char *argv[]) {
         u[i] = i;
     }
 
-    clock_t start_ticks = clock(); // cpu clock ticks
+    clock_t startTime = clock(); // cpu clock ticks
 
     // compute l2-norm of vector (array)
     double accum = 0.;
@@ -26,13 +26,9 @@ int main( int argc, char *argv[]) {
     }
     double norm = sqrt(accum);
 
-    clock_t end_ticks = clock();
+    clock_t endTime = clock();
+    clock_t cpuTime = endTime - startTime; // elapsed clock ticks
 
-    clock_t cpu_ticks = end_ticks - start_ticks; // elapsed clock ticks
-    double cpu_time_used = ((double) cpu_ticks) / CLOCKS_PER_SEC;
-
-    // printf("%ld\n", cpu_ticks);
-    printf("%f\n", cpu_time_used);
-
+    printf("%ld", cpuTime);
 }
 
