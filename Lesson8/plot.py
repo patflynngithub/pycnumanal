38435fe39a463900723a2102d10f5e1ab15b1bf4
Patrick Flynn <patflynn@hotmail.com>
2016-08-12 17:31:54 -0700

    Finished Lessons 7-8 code. Doing Lesson writeups

--- Lesson8/plot.py
+++ Lesson8/plot.py
@@ -5,7 +5,7 @@ import os
 import sqlite3
 import matplotlib.pyplot as plt
 
-print os.getcwd()
+print os.getcwd()  # current working directory
 
 db_filename     = 'timings.db'
 
@@ -18,21 +18,18 @@ if not db_exists:
 with sqlite3.connect(db_filename) as conn:
 
     # setup for querying timings database for desired timings
-    name         = 'l2 vector norm'
-
+    alg_name         = 'l2 vector norm'
     cur = conn.cursor()  # database table cursor
 
-#	select * from timings inner join algorithms on algorithms.name = timings.algorithm
-#	where algorithms.name = 'l2 vector norm';
+    # extract desired timings from timings table
+    cur.execute("SELECT problem_size, time FROM timings " +
+                "INNER JOIN algorithms ON algorithms.name = timings.algorithm WHERE algorithms.name = ? " + 
+                "ORDER BY problem_size ASC", (alg_name,))
+    timings_info = cur.fetchall() 
 
-    # insert timing into timings table
-    cur.execute("select problem_size, time from timings inner join algorithms on algorithms.name = timings.algorithm where algorithms.name = 'l2 vector norm'");
-
-#    c.execute("SELECT * FROM {tn} WHERE {idf}={my_id}".\
-#            format(tn=table_name, cn=column_2, idf=id_column, my_id=some_id))
-    timings_info = cur.fetchall()
     print timings_info
 
+    # organize timings info for plotting
     sizes   = [size[0]   for size   in timings_info]
     timings = [timing[1] for timing in timings_info]
 
@@ -42,5 +39,6 @@ with sqlite3.connect(db_filename) as conn:
     plt.plot(sizes, timings)
     plt.xlabel('problem size')
     plt.ylabel('seconds')
+    plt.title(alg_name)
     plt.show()
 
