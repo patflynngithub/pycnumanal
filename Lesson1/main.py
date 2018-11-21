2a1a13f14d2455801e11d35711a3151713c1f246
Patrick Flynn <patflynn@hotmail.com>
2018-11-21 10:15:40 -0800

    2018 Revison - Lesson 1

--- Lesson1/main.py
+++ Lesson1/main.py
@@ -1,8 +1,8 @@
-# single line comment
+# Python single line comment
 # main driver program in Python
-# will call l2vecnorm.c
+# will eventual call the l2vecnorm C program to get execution timing
 
-""" This is an example of a multiline
+""" This is an example of a Python multiline
 comment that spans multiple lines
 """
 
