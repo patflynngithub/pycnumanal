ed7872630177f0eaeb1f8273104b87c49345dea6
Patrick Flynn <patflynn@hotmail.com>
2018-11-23 09:35:57 -0800

    2018 Lesson 4 revision

--- Lesson4/main.py
+++ Lesson4/main.py
@@ -1,4 +1,8 @@
+# Python single line comment
 # main driver program in Python
-# will call l2vecnorm.c
+# will eventual call the l2vecnorm C program to get execution timing
 
+""" This is an example of a Python multiline
+comment that spans multiple lines
+"""
 
