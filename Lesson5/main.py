e460d3b550373c411c51832070d42427cfc1a312
Patrick Flynn <patflynn@hotmail.com>
2018-11-24 10:01:29 -0800

    2018 Lesson 5 revision

--- Lesson5/main.py
+++ Lesson5/main.py
@@ -2,10 +2,13 @@
 
 import os
 
-print os.getcwd()
+print(os.getcwd())
 
 external_command = "./l2vecnorm 40000"
+
+# calls external program with program size and
+# reads the returned timing value
 ret_value = os.popen(external_command).readlines()
 
-print ret_value
+print(ret_value)
 
