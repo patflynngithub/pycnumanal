a2d838ca77690ce1682f71405e38d46f26dfa61e
Patrick Flynn <patflynn@hotmail.com>
2018-12-11 17:51:13 -0800

    1st commit for new pycnumanal repository purpose

--- Lesson1/l2vecnorm.c
+++
@@ -1,5 +0,0 @@
-/* l2 vector norm */
-
-/* comments in C
-   can be multiline but not nested */
-
--- Lesson1/main.py
+++
@@ -1,8 +0,0 @@
-# Python single line comment
-# main driver program in Python
-# will eventual call the l2vecnorm C program to get execution timing
-
-""" This is an example of a Python multiline
-comment that spans multiple lines
-"""
-
